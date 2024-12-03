d3916105aab78ea302adca9a56cea3a51fdc161a
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2024-12-03 06:51:46 -0600

    feat(config): ✨ update log message

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 
-import { PORT } from './utils/constants/config.js'
+import { generateIdForEntity } from './utils/functions/generate-id-for-entity.js'
+import { PORT } from './utils/constants/index.js'
+import { showLog } from './utils/functions/show-log.js'
 
 const app = express()
 
 app.listen(PORT)
 
-console.log(`Server is running on port ${PORT}`)
+showLog({ message: `Server is running on port ${PORT}`, path: import.meta.filename })
+
+generateIdForEntity('messages')
